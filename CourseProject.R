235f926fdc341052d01feac3f31440131df90e33
John Cotter <john.cotter@daimler.com>
2014-06-22 20:36:49 -0700

    Updated for auto answer file generation

--- CourseProject.R
+++ CourseProject.R
@@ -45,3 +45,16 @@ fullCM$overall[1]-pcaCM$overall[1]
 finalRF<-randomForest(training$classe ~.,data = training,importance = TRUE)
 Answer<-predict(finalRF,testing)
 Answer
+
+# Submission for Course Project
+answers = rep("A", 20)
+  
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
+  
+pml_write_files(Answer)
